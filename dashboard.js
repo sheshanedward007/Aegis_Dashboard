5c2e6052375d11d9c324477aa88478a4c4c8adc6
sheshanedward007 <sheshanedward007@gmail.com>
2025-12-14 00:22:42 +0530

    Firebase bug fix

--- dashboard.js
+++ dashboard.js
@@ -2,6 +2,21 @@
 
 let incidents = [];
 
+// Firebase Configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBmxz5b598PlSbUyKbh4VwHvRpAxltqcFA",
+    authDomain: "project-aegis-845e6.firebaseapp.com",
+    projectId: "project-aegis-845e6",
+    storageBucket: "project-aegis-845e6.firebasestorage.app",
+    messagingSenderId: "851940559314",
+    appId: "1:851940559314:web:a2d93e9d366577855a200b"
+};
+
+// Initialize Firebase
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
+
 // Firebase Firestore reference
 const db = firebase.firestore();
 const reportsRef = db.collection("reports"); // Make sure this matches your Firestore collection
